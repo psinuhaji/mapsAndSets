5c5d695299c4c25ed9914cb63b8a9ebe0b136ea3
Phil <ps@email.com>
2022-08-07 15:00:34 -0700

    commit all

---
+++ mapsAndSets.js
@@ -0,0 +1,33 @@
+new Set([1,1,2,2,3,4]); // returns {1,2,3,4}
+
+[...new Set("referee")].join("") // returns 'ref'
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+/* Returns
+0: {Array(3) => true}
+1: {Array(3) => false} */
+
+const hasDuplicate = (arr) => {
+    return new Set(arr).size !== arr.length;
+}
+
+const isVowel = (char) => {
+    return "aeiou".includes(char);
+}
+
+const vowelCount = (str) => {
+    const vowelMap = new Map();
+    for(let char of str){
+        let lowerCaseChar = char.toLowerCase()
+        if(isVowel(lowerCaseChar)){
+            if(vowelMap.has(lowerCaseChar)){
+                vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+            } else {
+                vowelMap.set(lowerCaseChar, 1);
+            }
+        }
+    }
+    return vowelMap;
+}
